3069e73de65d76c2e24829a2cb5acbdc65b1174d
meghnaaxx <meghna.chawla134@gmail.com>
2023-12-01 19:48:47 +0530

    update

---
+++ image-gallery-php/include/container.php
@@ -0,0 +1,22 @@
+</head>
+<body>
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+  <div class="container">
+	<div class="navbar-header">
+	  <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a href="#" class="navbar-brand">Gallery System</a>
+	</div>
+	<div class="navbar-collapse collapse">
+	  <ul class="nav navbar-nav">
+		<li class="active"><a href="http://localhost/image-gallery-php/">Home</a></li>           
+	  </ul>         
+	</div>
+  </div>
+</div>
+	
+	
